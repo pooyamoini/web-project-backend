43ec46b38664ad1716952f2d527f5d13f36647ad
sepehrAmini <sepehramini83@gmail.com>
2020-01-30 13:09:54 +0330

    follow id changed to big integer

--- apps/follow/models.py
+++ apps/follow/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 class  AccountFollowType(models.Model):
+    id = models.BigAutoField(primary_key=True)
     follower_id = models.CharField(max_length = 15 , null= False )
     followed_id = models.CharField(max_length = 15 , null= False )
 
